2c92e0bb8a6ce78c1a7345bb30c7d95f5809931e
Jacob Ford <j@cobford.com>
2022-12-30 00:32:47 -0500

    Add the same config & plugins I used on wv-learningcenter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from 'astro/config';
+import rehypeToc from 'rehype-toc';
+import rehypeSlug from 'rehype-slug';
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  markdown: {
+    extendPlugins: 'astroDefaults',
+    rehypePlugins: [rehypeSlug, [rehypeToc, {
+      headings: ['h2', 'h3'],
+      cssClasses: {
+        toc: 'course-toc'
+      }
+    }]]
+  },
   integrations: [mdx()]
 });
